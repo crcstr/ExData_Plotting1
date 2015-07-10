9847887225284be1571a8db446f317722d428eeb
btamasi <balint.tamasi@gmail.com>
2015-07-10 11:10:47 +0200

    data manipulation, plots, gitignore

---
+++ plot1.R
@@ -0,0 +1,88 @@
+## Check data and download file if needed
+if (!file.exists("household_power_consumption.txt")) {
+    fileUrl <- paste0("https://d396qusza40orc.cloudfront.net/exdata",
+                      "%2Fdata%2Fhousehold_power_consumption.zip")
+    download.file(fileUrl, destfile = "power.zip", method = "curl")
+    unzip("power.zip")
+    file.remove("power.zip")
+}
+
+## Read data
+d <- read.table("household_power_consumption.txt",
+                sep = ";",
+                colClasses = c(rep("character", 2),
+                    rep("numeric", 7)),
+                na.strings = "?",
+                ##nrow = 5,
+                header = TRUE)
+
+d$datetime <- paste(d$Date, d$Time, sep = " ")
+d <- transform(d, Date = as.Date(Date, "%d/%m/%Y"),
+               datetime = strptime(datetime, "%d/%m/%Y %H:%M:%S"))
+
+## Subset
+d <- subset(d, Date %in% as.Date(c("2007/02/01", "2007/02/02")))
+
+## Plot 1
+png(filename = "plot1.png", width = 480, height = 480)
+hist(d$Global_active_power,
+     col = "red", border = "black",
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)")
+dev.off()
+
+## Plot 2
+png(filename = "plot2.png", width = 480, height = 480)
+with(d, plot(datetime, Global_active_power,
+             type = "l", col = "black",
+             ylab = "Global Active Power (kilowatts)",
+             xlab = ""))
+dev.off()
+
+## Plot 3
+png(filename = "plot3.png", width = 480, height = 480)
+with(d, {
+    plot(datetime, Sub_metering_1,
+         type = "l", col = "black",
+         ylab = "Energy Sub Metering",
+         xlab = "")
+    lines(datetime, Sub_metering_2,
+          type = "l", col = "red")
+    lines(datetime, Sub_metering_3,
+          type = "l", col = "blue")
+})
+legend("topright", lty = c(1, 1),
+       col = c("black", "red", "blue"),
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
+
+## Plot 4
+png(filename = "plot4.png", width = 480, height = 480)
+par(mfrow = c(2,2), cex = 0.75)
+with(d, {
+    plot(datetime, Global_active_power,
+         type = "l", col = "black",
+         ylab = "Global Active Power",
+         xlab = "")
+    
+    plot(datetime, Voltage,
+         type = "l", col = "black",
+         ylab = "Voltage")
+
+    plot(datetime, Sub_metering_1,
+         type = "l", col = "black",
+         ylab = "Energy Sub Metering",
+         xlab = "")
+    lines(datetime, Sub_metering_2,
+          type = "l", col = "red")
+    lines(datetime, Sub_metering_3,
+          type = "l", col = "blue")
+    legend("topright", lty = c(1, 1),
+           col = c("black", "red", "blue"),
+           legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+           bty = "n")
+
+    plot(datetime, Global_reactive_power,
+         type = "l", col = "black")
+})
+dev.off()
